30e2de258595f2853c15faa85981776a2b2bd89d
calebtd <caleb@dillenbeck.org>
2024-04-24 22:26:16 -0600

    Fixed lots of functionality issues, added some features

--- main.js
+++ main.js
@@ -1,40 +1,157 @@
-let lists = JSON.parse(localStorage.getItem("lists"));
+// let oldList = {
+//   'uuid': {
+//     name: "Initial List",
+//     items: [
+//       { content: "Add the first item", status: false },
+//       { content: "Make the footer blue", status: true },
+//     ],
+//   },
+//   2: {
+//     name: "Screens",
+//     items: [
+//       { content: "Built-in", status: true },
+//       { content: "2752H", status: true },
+//       { content: "S24E450", status: false },
+//     ],
+//   },
+// };
 
+// Save current 'allLists' object to Local Storage
 function save() {
-  localStorage.setItem("lists", JSON.stringify(lists));
+  localStorage.setItem("allLists", JSON.stringify(allLists));
 }
-// localStorage.clear();
 
-function createItemElement(id, value, status) {
+// Generate new list object within the 'allLists' object
+function createNewList(name) {
+  newID = crypto.randomUUID();
+  allLists[newID] = { name: name, todos: {} };
+  renderLists();
+}
+
+// Create a new item object within a given todo object
+function createNewItem() {
+  newUUID = crypto.randomUUID();
+  listID.todos.newUUID = { content: "", status: false };
+}
+
+function selectList(listID) {
+  selectedListID = listID;
+  renderLists();
+  renderSelectedList();
+}
+
+function renderLists() {
+  const sidebarDiv = document.getElementById("sidebar-div");
+  sidebarDiv.innerHTML = "";
+
+  for (let listID in allLists) {
+    const button = document.createElement("button");
+    button.setAttribute(
+      "class",
+      "px-2 py-1 mb-0.5 rounded w-full text-left hover:bg-neutral-600/25"
+    );
+    // button.setAttribute("id", listID);
+    button.setAttribute("onclick", `selectList('${listID}')`);
+    button.innerText = allLists[listID].name;
+
+    if (selectedListID === listID) {
+      button.classList.remove("hover:bg-neutral-600/25");
+      button.classList.add("bg-neutral-600/45");
+    }
+
+    sidebarDiv.appendChild(button);
+  }
+
+  save();
+}
+
+// This renders the items and such from the list that is selected
+function renderSelectedList() {
+  let selectedList = allLists[selectedListID];
+
+  let nameDiv = document.getElementById("selected-list-name");
+
+  // Define the span to control the dynamic width of the input box
+  let nameSpan = document.createElement("span");
+  nameSpan.setAttribute(
+    "class",
+    "absolute height h-0 overflow-hidden whitespace-pre"
+  );
+
+  let nameInput = document.createElement("input");
+  nameInput.setAttribute("class", "bg-inherit outline-offset-4");
+  nameInput.setAttribute("id", "name_" + selectedListID);
+  nameInput.setAttribute("type", "text");
+  nameInput.setAttribute("value", selectedList.name);
+  nameInput.setAttribute("disabled", "");
+
+  nameDiv.innerHTML = "";
+  nameDiv.appendChild(nameSpan);
+  nameDiv.appendChild(nameInput);
+
+  nameInput.addEventListener("input", resizeInput);
+  resizeInput();
+  function resizeInput() {
+    nameSpan.textContent = nameInput.value;
+    nameInput.style.width = nameSpan.offsetWidth + "px";
+  }
+
+  let todosDiv = document.getElementById("selected-list-todos");
+  todosDiv.innerHTML = "";
+  for (let item in selectedList.todos) {
+    itemElement = createItemElement(
+      item,
+      selectedList.todos[item].content,
+      selectedList.todos[item].status
+    );
+    todosDiv.appendChild(itemElement);
+  }
+  save();
+}
+
+//
+function renameSelectedList() {}
+
+// Create the element for each todo item
+function createItemElement(id, content, status) {
   const div = document.createElement("div");
   div.setAttribute("class", "flex items-center mb-2 border border-slate-400");
   div.setAttribute("id", id);
 
   const checkbox = document.createElement("input");
   checkbox.setAttribute("class", "m-2 peer");
+  checkbox.setAttribute("id", "checkbox_" + id);
   checkbox.setAttribute("type", "checkbox");
-  checkbox.setAttribute("id", "cb" + id);
-  checkbox.setAttribute("onclick", `boxChecked('${id}')`);
+  checkbox.setAttribute("onclick", `handleCheckbox('${id}')`);
 
   const input = document.createElement("input");
-  input.setAttribute("id", "i" + id);
   input.setAttribute(
     "class",
-    "peer-checked:line-through bg-inherit p-2 w-full outline-none"
+    "peer-checked:line-through bg-inherit m-1 p-1 w-full rounded outline-1"
   );
+  input.setAttribute("id", "input_" + id);
   input.setAttribute("type", "text");
+  input.setAttribute("value", content);
   input.setAttribute("disabled", "");
-  input.setAttribute("value", value);
-  input.setAttribute("onblur", `disableEditItem('${id}')`);
+  input.setAttribute("onblur", `saveItem('${id}')`);
 
   const editButton = document.createElement("button");
   editButton.setAttribute(
     "class",
     "bg-blue-500 hover:bg-blue-700 text-white p-1.5 px-3 rounded"
   );
-  editButton.setAttribute("onclick", `enableEditItem('${id}')`);
-  editButton.setAttribute("id", "b" + id);
+  editButton.setAttribute("id", "editButton_" + id);
+  editButton.setAttribute("onclick", `editItem('${id}')`);
   editButton.innerText = "Edit";
+
+  const saveButton = document.createElement("button");
+  saveButton.setAttribute(
+    "class",
+    "bg-blue-500 hover:bg-blue-700 text-white p-1.5 px-3 rounded hidden"
+  );
+  saveButton.setAttribute("id", "saveButton_" + id);
+  saveButton.setAttribute("onclick", `saveItem('${id}')`);
+  saveButton.innerText = "Save";
 
   const deleteButton = document.createElement("button");
   deleteButton.setAttribute("class", "p-1.5 mx-3 rounded hover:bg-slate-300");
@@ -46,103 +163,109 @@ function createItemElement(id, value, status) {
   if (status) {
     checkbox.setAttribute("checked", "");
   }
+  input.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      saveButton.click();
+    }
+  });
 
   div.appendChild(checkbox);
   div.appendChild(input);
   div.appendChild(editButton);
+  div.appendChild(saveButton);
   div.appendChild(deleteButton);
 
   return div;
 }
 
-// function renderLists() {
-//   const ul = document.getElementById("lists");
-//   if (lists.length > 0) {
-//     ul.innerHTML = "";
-//     for (let i = 0; i < lists.length; i++) {
-//       const li = document.createElement("li");
-//       li.appendChild(document.createTextNode(lists[i].name));
-//       ul.appendChild(li);
-//       console.log(ul);
-//     }
-//   } else {
-//     li.appendChild(document.createTextNode("New List"));
-//     ul.appendChild(li);
-//   }
-// }
-
-// function render() {
-//   for (let x in lists) {
-//     console.log(lists[x].name);
-//   }
-// }
-
-function render() {
-  // this will hold the html that will be displayed in the sidebar
-  let listsHtml = '<ul class="list-group">';
-  // iterate through the lists to get their names
-  for (let x in lists) {
-    listsHtml += `<li class="list-group-item">${lists[x].name}</li>`;
-  }
-  listsHtml += "</ul>";
-  // print out the lists
-  document.getElementById("lists").innerHTML = listsHtml;
-  // print out the name of the selected list
-  document.getElementById("selected-list-name").innerText = selectedList.name;
-  // iterate over the todos in the selected list
-  let todosDiv = document.getElementById("selected-list-todos");
-  for (let x in selectedList.items) {
-    let listItem = selectedList.items[x];
-    todosDiv.appendChild(
-      createItemElement(crypto.randomUUID, listItem.content, listItem.status)
-    );
+// Function that runs when a checkbox is checked
+function handleCheckbox(itemID) {
+  const checkBox = document.getElementById(`checkbox_${itemID}`);
+  item = allLists[selectedListID].todos[itemID];
+  if (checkBox.checked) {
+    item.status = true;
+  } else {
+    item.status = false;
   }
   save();
 }
 
-// function renderContent() {
-//   if (selected) {
-//     const content = document.getElementById("content");
-//     content.innerHTML = "";
-//     const ul = document.createElement("ul");
-//     let listItems = lists.selected.items;
-//     for (i = 0; i < listItems.length; i++) {
-//       const li = document.creaeElement("li");
-//       li.appendChild(document.createTextNode(listItems[i]));
-//       listItems[i];
-//     }
-//   }
-// }
-
-let selectedList = lists[1];
-render();
-
-// Example usage:
-// const golfCourseElement = createGolfCourseElement("01", "Testing Item");
-// document.body.appendChild(golfCourseElement);
-
-function boxChecked(id) {
-  const checkBox = document.getElementById(`cb${id}`);
-  if (checkBox.checked) {
-    // inputBox.value = "true";
-  } else {
-    // inputBox.value = "false";
-  }
-}
-
-function enableEditItem(id) {
-  const inputBox = document.getElementById(`i${id}`);
+// Function that runs when the 'Edit' button is pressed
+function editItem(itemID) {
+  const inputBox = document.getElementById(`input_${itemID}`);
   inputBox.disabled = false;
   let len = inputBox.value.length;
   inputBox.focus();
   inputBox.setSelectionRange(len, len);
-  document.getElementById(`b${id}`).innerHTML = "Save";
+  document.getElementById(`editButton_${itemID}`).classList.add("hidden");
+  document.getElementById(`saveButton_${itemID}`).classList.remove("hidden");
 }
 
-function disableEditItem(id) {
-  document.getElementById(`b${id}`).innerHTML = "Edit";
+// Function that runs when the 'Save' button is pressed or 'submit' event
+function saveItem(itemID) {
+  const inputBox = document.getElementById(`input_${itemID}`);
+  inputBox.disabled = true;
+  document.getElementById(`editButton_${itemID}`).classList.remove("hidden");
+  document.getElementById(`saveButton_${itemID}`).classList.add("hidden");
+
+  item = allLists[selectedListID].todos[itemID];
+  item.content = inputBox.value;
+  save();
 }
 
-function removeItem(id) {
-  document.getElementById(id).remove();
+// Function that runs when the 'X' delete button is pressed
+function removeItem(itemID) {
+  document.getElementById(itemID).remove();
+  delete allLists[selectedListID].todos[itemID];
+  save();
 }
+
+// -- CODE THAT RUNS --
+
+let passDefaultLists = false;
+let passSelectedList = true;
+
+let allLists = JSON.parse(localStorage.getItem("allLists"));
+
+let selectedListID = null;
+
+if (passDefaultLists) {
+  allLists = {
+    uuid1: {
+      name: "New List",
+      todos: {
+        uuid2: {
+          content: "First Item",
+          status: true,
+        },
+      },
+    },
+    uuid3: {
+      name: "Second List",
+      todos: {
+        uuid4: {
+          content: "My Todo Item",
+          status: false,
+        },
+        uuid5: {
+          content: "My next todo item",
+          status: true,
+        },
+      },
+    },
+  };
+}
+
+if (passSelectedList) {
+  selectedListID = Object.keys(allLists)[1];
+  renderSelectedList();
+}
+
+if (allLists == null) {
+  allLists = {};
+  createNewList("New List");
+}
+
+renderLists();
+
+// localStorage.clear();
